b64daf742eb37a81644c15854bbb24a90b0ce28d
Arman4102 <pinil1862@gmail.com>
2026-04-11 07:31:06 +0700

    deploy v2

--- backend/test_api.js
+++ backend/test_api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+// const API_BASE_URL = "http://localhost:5000/api";
+// Change to http://localhost:5000/api if testing locally
+const API_BASE_URL = "https://adapt-learn-be.vercel.app/api";
 
 async function testAPI() {
     console.log("checking if the api actually works...\n");
